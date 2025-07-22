c7fa6b16f655ac2eb9dd83ad337c56cf399e55d3
= <=>
2025-07-21 21:18:29 -0700

    more dev

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,39 +1,39 @@
-# module "shared_alb" {
-#   source = "./modules/application_load_balancer"
+module "shared_alb" {
+  source = "./modules/application_load_balancer"
 
-#   alb_name           = "dp-webapp"
-#   target_environment = local.target_environment
-#   vpc_id             = module.basic_vpc.vpc_id
-#   subnet_ids         = module.basic_vpc.public_subnet_ids
+  alb_name           = "dp-webapp"
+  target_environment = local.target_environment
+  vpc_id             = module.basic_vpc.vpc_id
+  subnet_ids         = module.basic_vpc.public_subnet_ids
 
-#   internal_alb = false # Set to false for an internet-facing ALB
+  internal_alb = false # Set to false for an internet-facing ALB
 
-#   # HTTP Listener (optional)
-#   http_enabled       = true
-#   http_port          = 80
+  # HTTP Listener (optional)
+  http_enabled = true
+  http_port    = 80
 
-#   # allow Internet traffic in
-#   http_ingress_cidrs = ["0.0.0.0/0"]
+  # allow Internet traffic in
+  http_ingress_cidrs = ["0.0.0.0/0"]
 
-#   # HTTPS Listener (optional)
-#   https_enabled = false
+  # HTTPS Listener (optional)
+  https_enabled = false
 
-#   # ALB Access Logging (optional)
-#   enable_access_logs                       = true
-#   access_logs_s3_bucket_name               = local.alb_log_bucket_name
-#   access_logs_s3_prefix                    = local.project_name
-#   access_logs_s3_bucket_versioning_enabled = true
-#   access_logs_s3_bucket_lifecycle_enabled  = true
-#   ### access_logs_s3_bucket_transition_days    = 30 # Transition logs to IA after 30 days
-#   access_logs_s3_bucket_expiration_days = 60 # Delete logs after 60 days
+  # ALB Access Logging (optional)
+  enable_access_logs                       = true
+  access_logs_s3_bucket_name               = local.alb_log_bucket_name
+  access_logs_s3_prefix                    = local.project_name
+  access_logs_s3_bucket_versioning_enabled = true
+  access_logs_s3_bucket_lifecycle_enabled  = true
+  ### access_logs_s3_bucket_transition_days    = 30 # Transition logs to IA after 30 days
+  access_logs_s3_bucket_expiration_days = 60 # Delete logs after 60 days
 
-#   tags = local.common_tags
-# }
+  tags = local.common_tags
+}
 
-# output "alb_dns_name" {
-#   value = module.shared_alb.alb_dns_name
-# }
+output "alb_dns_name" {
+  value = module.shared_alb.alb_dns_name
+}
 
-# output "alb_security_group_id" {
-#   value = module.shared_alb.alb_security_group_id
-# }
+output "alb_security_group_id" {
+  value = module.shared_alb.alb_security_group_id
+}
--- terraform/launch_template.tf
+++ terraform/launch_template.tf
@@ -2,7 +2,7 @@ module "ec2_launch_template" {
   source = "./modules/launch_template" # Adjust the path to your module
 
   terraform_project_name = local.project_name
-  ec2_template_prefix = "${local.project_name}-${local.target_environment}"
+  ec2_template_prefix    = "${local.project_name}-${local.target_environment}"
 
   create_ssm_instance_profile = true
   # vpc_security_group_ids = ["sg-0abcdef1234567890", "sg-0fedcba9876543210"]
